6aee7fb7128cfa45948d1f34fa440473fb05c1ec
muad0dib <iamkofiantwi@outlook.com>
2022-01-02 19:40:39 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+this is the first thing that i am making in git. I am turning up a new chapter in 2022
+hopefully, I hope i follow till the end
---
+++ integers.py
@@ -0,0 +1,9 @@
+print(6+2)
+print(10-2)
+print(16/2)
+print(4*2)
+
+a = 56
+favorite_num = 'My favorite number is'
+print(favorite_num)
+print(a)
